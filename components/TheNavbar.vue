5d19b1083edcf3b495df622a761b8544d9684ba2
guoqiang1 <18701094030@163.com>
2025-03-31 10:45:22 +0800

    修复语言切换问题

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -19,7 +19,6 @@
         </div>
         <!-- 语言切换 -->
         <div class="flex items-center">
-          {{ locale }}
           <select
             v-model="selectedLocale"
             class="hidden sm:block ml-4 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
